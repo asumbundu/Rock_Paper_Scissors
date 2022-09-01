5ec5fc8e7ba3c14a56d7494c8adf0a934718faf6
Ali Sumbundu <alisumbundu@Alis-MBP.lan>
2022-08-31 22:31:05 -0500

    Updated getComputeChoice function

--- external.js
+++ external.js
@@ -1,23 +1,34 @@
 function getComputerChoice() {
   str = ["rock", "paper", "scissors"];
   let index = Math.floor(Math.random() * str.length);
-  let selection = str[index];
-  console.log(selection);
+  let selectionR = str[index];
+  let selectionL = selectionR.toLowerCase();
+  return selectionL;
 }
 let computerSelection = getComputerChoice();
 
-let playerSelection = prompt("Rock", "Paper", "Scissors");
-
+let SelectionP = prompt("rock", "paper", "scissors");
+let playerSelection = SelectionP.toLowerCase();
+//console.log(playerSelection);
+//console.log(computerSelection);
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "paper")
-    result = "You lost! Paper Wins";
-  else if (playerSelection === "rock" && computerSelection === "scissors") {
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    result = "You won! Scissors beat paper";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    result = "You lost! Scissors beat paper";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    result = "You lost! Rock beats Scissors";
-  } else result = "Game over";
+  if (playerSelection == "rock" && computerSelection == "paper") {
+    // (playerSelection == "rock" && computerSelection == "paper")
+    //console.log("You lost! Paper Wins");
+    return "You lost! Paper Wins";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You won! Rock beats Scissors";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You won! Scissors beat paper";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You lost! Scissors beat paper";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You lost! Rock beats Scissors";
+  } else {
+    return "Game over";
+  }
 }
+
+//console.log(playerSelection);
+//console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
