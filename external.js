6069f6ca84d904a97ceebb6c42a2e96c363619cd
Ali Sumbundu <alisumbundu@Alis-MBP.lan>
2022-09-03 00:06:33 -0500

    Updated the counter function

--- external.js
+++ external.js
@@ -6,7 +6,8 @@ function getComputerChoice() {
   return selectionL;
 }
 let computerSelection = getComputerChoice();
-
+let PlayerScore = 0;
+let ComputerScore = 0;
 let SelectionP = prompt("rock", "paper", "scissors");
 let playerSelection = SelectionP.toLowerCase();
 //console.log(playerSelection);
@@ -15,15 +16,26 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "paper") {
     // (playerSelection == "rock" && computerSelection == "paper")
     //console.log("You lost! Paper Wins");
-    return "You lost! Paper Wins";
+    //return "You lost! Paper Wins";
+    return "Player wins";
+    //ComputerScore++;
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    return "You won! Rock beats Scissors";
+    return "Player wins";
+    //return "You won! Rock beats Scissors";
+    // PlayerScore++;
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return "You won! Scissors beat paper";
+    //return "You won! Scissors beat paper";
+    return "Player wins";
+
+    // PlayerScore++;
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    return "You lost! Scissors beat paper";
+    return "Compputer wins";
+    //return "You lost! Scissors beat paper";
+    //ComputerScore++;
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    return "You lost! Rock beats Scissors";
+    return "Computer wins";
+    //return "You lost! Rock beats Scissors";
+    // ComputerScore++;
   } else {
     return "Game over";
   }
@@ -31,4 +43,21 @@ function playRound(playerSelection, computerSelection) {
 
 //console.log(playerSelection);
 //console.log(computerSelection);
+//console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    //let playerSelection = SelectionP.toLowerCase();
+  
+    playRound(playerSelection, computerSelection);
+    if (playRound(playerSelection, computerSelection) == "Player wins")
+      PlayerScore++;
+    else if (playRound(playerSelection, computerSelection) == "Computer wins")
+      ComputerScore++;
+      else 
+      return 
+  }
+}
 console.log(playRound(playerSelection, computerSelection));
+console.log(PlayerScore);
+console.log(ComputerScore);
